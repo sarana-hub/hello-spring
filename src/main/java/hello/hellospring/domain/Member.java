d2f4b862a0d1921dceec821bea00a35175e02835
nayeon-jo <nyc0516@naver.com>
2022-04-14 18:55:00 +0900

    회원도메인, 리포지토리

--- src/main/java/hello/hellospring/domain/Member.java
+++ src/main/java/hello/hellospring/domain/Member.java
@@ -7,27 +7,24 @@ import javax.persistence.Id;
 
 /** 회원 객체*/
 
-@Entity
+/*@Entity*/
 public class Member {
-
+    /*
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private long id;
-
+     */
+    private Long id;
     private String name;
 
-    public long getId() {
+    public Long getId() {
         return id;
     }
-
     public void setId(long id) {
         this.id = id;
     }
-
     public String getName() {
         return name;
     }
-
     public void setName(String name) {
         this.name = name;
     }
--- src/main/java/hello/hellospring/repository/MemberRepository.java
+++ src/main/java/hello/hellospring/repository/MemberRepository.java
@@ -6,11 +6,14 @@ import java.util.List;
 import java.util.Optional;
 
 /** 회원 리포지토리 인터페이스*/
-
-
 public interface MemberRepository {
     Member save(Member member);
+
     Optional<Member> findById(Long id);
     Optional<Member> findByName(String name);
+    /* Optional
+    *  값이 null이면
+   * */
+
     List<Member> findAll();
 }
