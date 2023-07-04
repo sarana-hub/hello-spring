d013176a2c25965c5b75c52646b1a278935b054c
nayeon-jo <nyc0516@naver.com>
2023-07-04 17:06:40 +0900

    회원 웹 기능 - 홈 화면 추가

--- src/main/java/hello/hellospring/controller/HomeController.java
+++ src/main/java/hello/hellospring/controller/HomeController.java
@@ -3,12 +3,12 @@ package hello.hellospring.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
-/** 홈 화면 추가 -홈 컨트롤러 */
-
 @Controller
 public class HomeController {
     @GetMapping("/")
     public String home() {
+
         return "home";
     }
 }
+/** 홈 화면 추가 */
