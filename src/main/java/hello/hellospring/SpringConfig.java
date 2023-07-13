54cb0bd9c7520bc254ee31fa0217d9998504e30c
nayeon-jo <nyc0516@naver.com>
2023-07-13 22:48:32 +0900

    스프링 데이터 JPA

--- src/main/java/hello/hellospring/SpringConfig.java
+++ src/main/java/hello/hellospring/SpringConfig.java
@@ -14,23 +14,25 @@ import javax.sql.DataSource;
 @Configuration
 public class SpringConfig {
 
-    private EntityManager em;
-
-    public SpringConfig(EntityManager em) {
-        this.em = em;
+    private final MemberRepository memberRepository;
+    //@Autowired  //생성자 하나만 있을땐 생략 가능
+    public SpringConfig(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
     }
+
+    //private EntityManager em;
+    //public SpringConfig(EntityManager em) {this.em = em;}
 
     @Bean
     public MemberService memberService() {
-
-        return new MemberService(memberRepository());
+        return new MemberService(memberRepository);
     }
 
-    @Bean
+    /*@Bean
     public MemberRepository memberRepository() {
         //return new MemoryMemberRepository();
         //return new JdbcMemberRepository(dataSource);
         //return new JdbcTemplateMemberRepository(dataSource);
         return new JpaMemberRepository(em);
-    }
+    }*/
 }
--- src/main/java/hello/hellospring/repository/SpringDataJpaMemberRepository.java
+++ src/main/java/hello/hellospring/repository/SpringDataJpaMemberRepository.java
@@ -1,4 +1,3 @@
-/*
 package hello.hellospring.repository;
 
 import hello.hellospring.domain.Member;
@@ -6,13 +5,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
-*/
-/**스프링 데이터 JPA 회원 리포지토리*//*
 
-
-public interface SpringDataJpaMemberRepository extends
-        JpaRepository<Member, Long>, MemberRepository {
+public interface SpringDataJpaMemberRepository extends JpaRepository<Member, Long>, MemberRepository {
 
     Optional<Member> findByName(String name);
 }
-*/
