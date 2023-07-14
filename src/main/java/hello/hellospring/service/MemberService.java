a8229f65af7027a0217b4a9e4b7cb3ae2d15c99e
nayeon-jo <nyc0516@naver.com>
2023-07-14 21:21:36 +0900

    AOP가 필요한 상황

--- src/main/java/hello/hellospring/service/MemberService.java
+++ src/main/java/hello/hellospring/service/MemberService.java
@@ -11,7 +11,6 @@ import org.springframework.transaction.annotation.Transactional;
 import java.util.List;
 import java.util.Optional;
 
-/** 회원 서비스*/
 
 
 @Transactional
@@ -20,7 +19,6 @@ public class MemberService {
     private final MemberRepository memberRepository;
 
     public MemberService(MemberRepository memberRepository) {
-
         this.memberRepository = memberRepository;
     }
 
@@ -28,49 +26,21 @@ public class MemberService {
      * 회원가입
      */
     public Long join(Member member) {
-        validateDuplicateMember(member);  //중복회원(같은이름회원) 검증
-        memberRepository.save(member);
-        return member.getId();
-    }
+        long start = System.currentTimeMillis();
 
-    private void validateDuplicateMember(Member member) {
-        memberRepository.findByName(member.getName())
-                .ifPresent(m -> {   //리포지토리에 값(같은이름)이 있으면
-                    throw new IllegalStateException("이미 존재하는 회원입니다.");
-                });
-    }
-
-
-    /**
-     * 전체 회원 조회
-     */
-    public List<Member> findMembers() {
-        return memberRepository.findAll();
-    }
-
-    public Optional<Member> findOne(Long memberId) {
-        return memberRepository.findById(memberId);
-    }
-}
-
-    /**
-     * 회원가입
-     *//*
-    public Long join(Member member) {
-        long start=System.currentTimeMillis();
-
-        try{
-            validateDuplicateMember(member);  //Extract method로 자동생성
+        try {
+            validateDuplicateMember(member);
 
             memberRepository.save(member);
             return member.getId();
-        }finally {
-            long finish=System.currentTimeMillis();
-            long timeMs=finish-start;
-            System.out.println("join="+timeMs+"ms");
+        } finally {
+            long finish = System.currentTimeMillis();
+            long timeMs = finish - start;
+            System.out.println("join " + timeMs + "ms");
         }
 
     }
+
     private void validateDuplicateMember(Member member) {
         //중복회원(같은이름회원) 검증
         memberRepository.findByName(member.getName())
@@ -79,21 +49,23 @@ public class MemberService {
                 });
     }
 
-    *//**
+    /**
      * 전체 회원 조회
-     *//*
+     */
     public List<Member> findMembers() {
-        long start=System.currentTimeMillis();
+        long start = System.currentTimeMillis();
+
         try {
             return memberRepository.findAll();
         } finally {
-            long finish=System.currentTimeMillis();
-            long timeMs=finish-start;
+            long finish = System.currentTimeMillis();
+            long timeMs = finish - start;
             System.out.println("findMembers " + timeMs + "ms");
         }
     }
 
     public Optional<Member> findOne(Long memberId) {
         return memberRepository.findById(memberId);
-    }*/
+    }
+}
 
