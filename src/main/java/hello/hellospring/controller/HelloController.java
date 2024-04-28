bab3d89163f5dac1751e9abd360cc19fa33741c5
nayeon-jo <nyc0516@naver.com>
2024-04-28 12:41:12 +0900

    스프링 웹 개발 세가지 방식 - 정적 컨텐츠, MVC와 템플릿 엔진, API

--- src/main/java/hello/hellospring/controller/HelloController.java
+++ src/main/java/hello/hellospring/controller/HelloController.java
@@ -16,37 +16,38 @@ public class HelloController {
         // viewResolver가 view(templates/hello.html)을 찾아서 처리
     }
 
+    /**MVC와 템플릿엔진 - 뷰리졸버로 html 반환*/
     @GetMapping("hello-mvc")
     public String helloMvc(@RequestParam("name") String name, Model model) {
         model.addAttribute("name", name);   // model(name:spring)
         return "hello-template";
+        //@RequestParam 값(name 값) 필수! http://localhost:8080/hello-mvc?name=spring
+        // http://localhost:8080/hello-mvc 실행하면 에러
     }
 
 
-    /** API - @ResponseBody 문자 반환*/
+    /**API - 메시지컨버터로 데이터 반환*/
+    /* @ResponseBody 문자 반환*/
     @GetMapping("hello-string")
-    @ResponseBody   //HTTP의 BODY에 문자 내용("hello spring")을 직접 반환
+    @ResponseBody   //HTTP의 BODY에 문자 내용("hello spring")을 직접 반환*/
     public String helloString(@RequestParam("name") String name) {
-
-        return "hello " + name; //"hello spring"
+        return "hello " + name; //"hello spring" //html없이 그냥 문자 그대로
     }
 
-    /** API - @ResponseBody 객체 반환*/
+    /* @ResponseBody 객체 반환*/
     @GetMapping("hello-api")
-    @ResponseBody    //객체가 JSON으로 변환됨
+    @ResponseBody    //반환한 객체가 JSON으로 변환됨
     public Hello helloApi(@RequestParam("name") String name) {
         Hello hello = new Hello();
         hello.setName(name);
-        return hello;
+        return hello;  //html없이 그냥 데이터 그대로
     }
     static class Hello{
         private String name;
         public String getName() {
-
             return name;
         }
         public void setName(String name) {
-
             this.name = name;
         }
     }
