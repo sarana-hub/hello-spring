794246676c358a82cf0188eced8b6301b1c94766
nayeon-jo <nyc0516@naver.com>
2024-05-19 11:57:57 +0900

    회원리포지토리

--- src/main/java/hello/hellospring/repository/MemoryMemberRepository.java
+++ src/main/java/hello/hellospring/repository/MemoryMemberRepository.java
@@ -6,14 +6,14 @@ import org.springframework.stereotype.Repository;
 import java.util.*;
 /** 회원 리포지토리 메모리 구현체*/
 
-/*
- * 동시성 문제가 고려되어 있지 않음, 실무에서는 ConcurrentHashMap, AtomicLong 사용 고려
- */
 
 public class MemoryMemberRepository implements MemberRepository {
 
     private static Map<Long, Member> store = new HashMap<>();
     private static long sequence = 0L;
+    /*
+     * 동시성 문제가 고려되어 있지 않음, 실무에서는 ConcurrentHashMap, AtomicLong 사용 고려
+     */
 
     @Override
     public Member save(Member member) {
@@ -24,7 +24,7 @@ public class MemoryMemberRepository implements MemberRepository {
 
     @Override
     public Optional<Member> findById(Long id) {
-        //return store.get(id);  //을 값이 null이어도 반환할수있도록
+        //return store.get(id);  을 값이 null이어도 반환할수있도록
         return Optional.ofNullable(store.get(id));
     }
 
